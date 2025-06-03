32fca0c05edfa8d66220a9306e724671d18a0e23
tedlee21 <jaywnl92@gmail.com>
2025-06-02 21:11:40 -0700

    updated taskSlice.js with proper camelCase naming

---
+++ frontend/src/slices/taskSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  taskList : []
+};
+
+const taskSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, action) => {
+      state.taskList.push(action.payload);
+    },
+    removeTask: (state, action) => {
+        // TODO
+    }
+  }
+});
+
+export const { addTask, removeTask } = taskSlice.actions;
+
+export const selectTaskList = (state) => state.tasks.taskList;
+
+export default taskSlice.reducer;
