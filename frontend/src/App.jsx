1c25c4ed084ba9335fe5fc4a26d6485483f49da0
Alex Luo <alexluo602@gmail.com>
2025-06-02 21:01:51 -0700

    Imported to Vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,4 @@
-import React from 'react';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { Provider } from 'react-redux';
-import { store } from './store';
-
 import HomePage from './components/HomePage';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
@@ -46,11 +40,10 @@ const router = createBrowserRouter([
   // TODO: Add other routes as needed
 ]);
 
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>
-  </Provider>
+function App() {
+  return (
+    <RouterProvider router={router} />
+  )
+}
 
-)
+export default App
