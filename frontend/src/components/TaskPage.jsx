f9cf02ee1e4b5cbb1148894102beaa9ef2950ec5
Kathrina Pillay <katkp@students.ubc.ca>
2025-06-03 19:14:21 -0700

    Fix resizing on task page

--- frontend/src/components/TaskPage.jsx
+++ frontend/src/components/TaskPage.jsx
@@ -5,22 +5,21 @@ import Stack from '@mui/material/Stack';
 import BookPage from "./tasks/BookPage";
 import AddTaskButton from "./tasks/AddTaskButton";
 
-
 export default function TaskPage() {
   return (
-        <Box className="Task">
+        <Box>
           <NavBar />
-          <Container sx={{ m: 4 }}>
-            <Stack direction="row" spacing={2} >
-            <BookPage>
-              <Stack direction="column" spacing={2} sx={{ width:"100%", height:"100%", display: "flex" }}>
-                <Typography variant="h4">My Tasks</Typography>
-                <AddTaskButton />
-              </Stack>
-            </BookPage>
-            <BookPage>
-              <Typography variant="h4">My Metrics</Typography>
-            </BookPage>
+          <Container maxWidth="false" sx={{bgcolor: "black", height:"85vh"}}>
+            <Stack direction="row" spacing={5} sx={{height: "100%", px:10, py:5, bgcolor: "pink", boxSizing: "border-box"}}>
+              <BookPage>
+                <Stack direction="column" spacing={2} sx={{ width:"100%", height:"85vh", display: "flex"}}>
+                  <Typography variant="h6">My Tasks</Typography>
+                  <AddTaskButton />
+                </Stack>
+              </BookPage>
+              <BookPage>
+                <Typography variant="h6">My Metrics</Typography>
+              </BookPage>
             </Stack>
           </Container>
         </Box>
--- frontend/src/components/tasks/BookPage.jsx
+++ frontend/src/components/tasks/BookPage.jsx
@@ -4,13 +4,15 @@ import Box from '@mui/material/Box';
 export default function BookPage({children}) {
   return (
     <Box sx={{
+          flex: 1,
           width: "50%",
-          height: 600,
+          height: "100%",
           borderRadius: 15,
           bgcolor: "tan",
-          p: 6,
+          p: 5,
           display: 'flex',
           justifyContent: 'center',
+          boxSizing: "border-box"
         }}> 
         {children}</Box>
   );
