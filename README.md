d672d4cfd14eba9b4fc252c687c58efa81c82645
tedlee21 <jaywnl92@gmail.com>
2025-06-03 22:06:07 -0700

    Integrated React Three Fiber into Home Page
    Added a placeholder 3D model of a cafe to the home
    page using React Three Fiber, along with camera
    controls and basic lighting.
    Refactored buttons on the home page to a separate
    HUD component for better organization and
    maintainability.
    Updated README with format for M1 requirements.

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # Team Loftly (Room to Grow)
-- Aryan Sonik
-- Alex Luo
 - Ted Lee
+- Alex Luo
 - Kathrina Pillay
+- Aryan Sonik
 
   
-Our project is a productivity application where users can creata and complete tasks to earn points and decorate a virtual room space by spending points to purchase decorations.
+Introducing Room to Grow, a productivity web app where users can create and complete tasks to build up their very own virtual study room.
+Earn coins and spend them at the Marketplace to buy various decorations for your room! Every minute of focus counts!
 
-# To build the frontend:
+## Build Instructions:
 - cd frontend
 - docker login (if you're not logged in)
 - docker build -t frontend .
 - docker run -p 3000:3000 frontend
-- access the site at: http://localhost:3000
+- access the site at: http://localhost:3000.
 
-Right now, this is just a react template project.
+## M1
+
+### Home Page
+- Added three.js scene with placeholder 3D room model
+- Room is rotation and zoom enabled through OrbitControls from the drei import
+- Basic lighting and camera settings have been set
+- Displays a seperate component for HUD buttons for different features 
+- UI styling done through tailwindCSS
--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,16 +1,43 @@
-import { useNavigate } from "react-router-dom";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
 import NavBar from "./NavBar";
+import HUD from "./home/HUD";
+import PlaceholderCafe from "../models/PlaceholderCafe";
+import "../css/HomePage.css";
+
 export default function HomePage() {
-    const navigate = useNavigate();
+  const adjustForScreenSize = () => {
+    const isSmallScreen = window.innerWidth < 800;
+    return {
+      scale: isSmallScreen ? [0.9, 0.9, 0.9] : [30, 30, 30],
+      position: [0, -15, 0],
+    };
+  };
+
+  const { scale: roomScale, position: roomPosition } = adjustForScreenSize();
+
   return (
-    <div className="Home">
-      <p>Home Page</p>
-      <NavBar/>
-      <button onClick={() => {navigate('/tasks')}}>My tasks</button>
-      <button onClick={() => {navigate('/stats')}}>My stats</button>
-      <button onClick={() => {navigate('/marketplace')}}>Marketplace</button>
-      <button onClick={() => {navigate('/edit')}}>Edit</button>
-      <button onClick={() => {/*TODO startup timer on home page or create another page*/}}>Timer</button>
-    </div>
+    <section className="w-full h-screen relative">
+      <NavBar />
+      <HUD />
+      <Canvas
+        camera={{ position: [-75, 60, 75], fov: 60 }}
+        className="w-full h-screen bg-gray-800"
+      >
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+        <PlaceholderCafe scale={roomScale} position={roomPosition} />
+        <OrbitControls
+          target={[0, 0, 0]}
+          minDistance={15}
+          maxDistance={150}
+          makeDefault
+          enablePan={false}
+          enableZoom
+          enableRotate
+        />
+      </Canvas>
+    </section>
   );
 }
