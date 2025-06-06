bd5e088610d6cac0b780feaec5fd2b03455d1027
Alex Luo <alexluo602@gmail.com>
2025-06-06 02:48:43 -0700

    Created skeleton for market

--- frontend/src/pages/Market.jsx
+++ frontend/src/pages/Market.jsx
@@ -1,7 +1,50 @@
-export default function Market() {
+import { Stack, Box, Paper, Divider, Typography } from "@mui/material";
+
+function Market() {
   return (
-    <div className="Market">
-      <p>Market</p>
-    </div>
+    <Stack
+      direction="row"
+      divider={
+        <Divider
+          orientation="vertical"
+          flexItem
+          sx={{ borderColor: "black", borderWidth: "1px" }}
+        />
+      }
+      sx={{
+        height: "100%",
+        boxSizing: "border-box",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          bgcolor: "grey.100",
+          m: 3,
+        }}
+      >
+        <Typography variant="h4">Left Component</Typography>
+      </Box>
+
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          bgcolor: "grey.200",
+          m: 3,
+        }}
+      >
+        <Typography variant="h4">Right Component</Typography>
+      </Box>
+    </Stack>
   );
 }
+
+export default Market;
