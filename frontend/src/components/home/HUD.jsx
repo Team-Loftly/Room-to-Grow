69cae78a390ba64b2b3c9b722409fb415e78a7b4
katkp <katkp@students.ubc.ca>
2025-07-10 22:54:30 -0700

    Redesigned HUD on homepage

--- frontend/src/components/home/HUD.jsx
+++ frontend/src/components/home/HUD.jsx
@@ -1,44 +1,73 @@
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { openTimer } from "../../features/timerSlice";
+import { Stack, IconButton } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Tooltip, { tooltipClasses } from "@mui/material/Tooltip";
+import HabitsIcon from "@mui/icons-material/ChecklistRtl";
+import MarketplaceIcon from "@mui/icons-material/Storefront";
+import EditRoomIcon from "@mui/icons-material/Construction";
+import TimerIcon from "@mui/icons-material/AccessAlarm";
+
+const CustomTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} placement="right" classes={{ popper: className }} />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "white",
+    color: "black",
+    fontSize: 18,
+    fontFamily: "Be Vietnam Pro",
+    fontWeight: 300,
+  },
+}));
 
 export default function HUD() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   return (
-    <div className="flex top-0 left-0 w-full h-16 flex justify-around items-center text-white text-bold ">
-      <button
-        onClick={() => {
-          navigate("/tasks");
-        }}
-        className="hover:cursor-pointer hover:transform hover:scale-120 transition-transform duration-200"
-      >
-        Habits
-      </button>
-      <button
-        onClick={() => {
-          navigate("/marketplace");
-        }}
-        className="hover:cursor-pointer hover:transform hover:scale-120 transition-transform duration-200"
-      >
-        Marketplace
-      </button>
-      <button
-        onClick={() => {
-          navigate("/edit");
-        }}
-        className="hover:cursor-pointer hover:transform hover:scale-120 transition-transform duration-200"
-      >
-        Edit Room
-      </button>
-      <button
-        onClick={() => {
-          dispatch(openTimer());
-        }}
-        className="hover:cursor-pointer hover:transform hover:scale-120 transition-transform duration-200"
-      >
-        Timer
-      </button>
-    </div>
+    <Stack
+      spacing={4}
+      sx={{
+        justifyContent: "center",
+        ml: 6,
+      }}
+    >
+      <CustomTooltip title="Habits">
+        <IconButton
+          onClick={() => {
+            navigate("/tasks");
+          }}
+        >
+          <HabitsIcon sx={{ color: "white", fontSize: 40 }} />
+        </IconButton>
+      </CustomTooltip>
+      <CustomTooltip title="Marketplace">
+        <IconButton
+          onClick={() => {
+            navigate("/marketplace");
+          }}
+        >
+          <MarketplaceIcon sx={{ color: "white", fontSize: 40 }} />
+        </IconButton>
+      </CustomTooltip>
+      <CustomTooltip title="Edit Room">
+        <IconButton
+          onClick={() => {
+            navigate("/edit");
+          }}
+        >
+          <EditRoomIcon sx={{ color: "white", fontSize: 40 }} />
+        </IconButton>
+      </CustomTooltip>
+      <CustomTooltip title="Timer">
+        <IconButton
+          onClick={() => {
+            dispatch(openTimer());
+          }}
+        >
+          <TimerIcon sx={{ color: "white", fontSize: 40 }} />
+        </IconButton>
+      </CustomTooltip>
+    </Stack>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,23 +2,22 @@ import HUD from "../components/home/HUD";
 import TimerPopup from "../components/home/timerPopup";
 import Room from "../components/Room.jsx";
 import { useSelector } from "react-redux";
-import { Box } from "@mui/material";
+import { Stack } from "@mui/material";
 import "../css/Home.css";
 
 export default function Home() {
   const showTimer = useSelector((state) => state.timer.showTimer);
   return (
-    <Box
+    <Stack
+      direction="row"
       sx={{
         boxSizing: "border-box",
         height: "100%",
-        display: "flex",
-        flexDirection: "column",
       }}
     >
       <HUD />
       {showTimer && <TimerPopup open={showTimer} />}
       <Room isEditable={false} />
-    </Box>
+    </Stack>
   );
 }
